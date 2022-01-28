aaf91f1e52620b46f0e6481ce818c38a5e2352cc
martinrendon <82111053+martinrendon@users.noreply.github.com>
2022-01-28 12:07:31 -0600

    Add files via upload

---
+++ public/home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="Mar bank"
+      title="Welcome"
+      text="You can move around using the navigation bar."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />
+  );  
+}
